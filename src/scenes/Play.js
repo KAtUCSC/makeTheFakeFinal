2dcf985eebc06c6a9b1f0d5065cb208b073f75db
KAtUCSC <smway@ucsc.edu>
2024-03-11 10:02:15 -0700

    world border implemented and map size changed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,6 +9,7 @@ class Play extends Phaser.Scene {
     //happens once every time the scene is restarted/added
     init() {
         //can recieve data
+        this.levelNumber = 1
     }
 
     //load assets
@@ -19,33 +20,57 @@ class Play extends Phaser.Scene {
     //make things
     create() {
         //can recieve data
+        //scaling factor
+        const globalScaleFactor = 4
         //tilemap setup
-        const map = this.add.tilemap('tilemapJSON1')
+        const map = this.add.tilemap(`tilemapJSON${this.levelNumber}`)
         //ground is the tileset's name in tiled
         const tileset = map.addTilesetImage('ground', 'assetImage')
         //foreground is the name of the layer in tiled
-        const groundLayer = map.createLayer('foreground', 'ground', 0, 0).setScale(4)
+        const groundLayer = map.createLayer('foreground', 'ground', 0, 0).setScale(globalScaleFactor)
         groundLayer.setCollisionByExclusion([-1, 0])
 
+        //find flag and bike pos
+        const bikeSpawn = map.findObject('spawns', (obj) => obj.name === 'bikeSpawn')
+        //console.log(bikeSpawn)
+        const flagSpawn = map.findObject('spawns', (obj) => obj.name === 'flagSpawn')
+
         //sky background
-        let sky = this.add.rectangle(0, 0, map.widthInPixels*4, map.heightInPixels*4, 0x00ff00).setOrigin(0).setDepth(-2)
-        console.log(sky)
+        let sky = this.add.rectangle(0, 0, map.widthInPixels*globalScaleFactor, map.heightInPixels*globalScaleFactor, 0xaaeeff).setOrigin(0).setDepth(-2)
+        //console.log(sky)
 
         //create bike and player
-        this.bike = this.matter.add.sprite(0, 0, 'sSheet', 'bike').setScale(4)
+        this.bike = this.matter.add.sprite(bikeSpawn.x*globalScaleFactor, bikeSpawn.y*globalScaleFactor, 'sSheet', 'bikeWheeled').setScale(globalScaleFactor)
 
         //colliders
-        this.matter.world.convertTilemapLayer(groundLayer)
-        
+        this.groundCollider = this.matter.world.convertTilemapLayer(groundLayer, )
+        console.log(this.groundCollider)
+
+        //set physics bounds
+        this.matter.world.setBounds(0, 0, map.widthInPixels*globalScaleFactor, map.heightInPixels*globalScaleFactor)
+        //world is small, make larger and taller
         
         //set camera bounds
-        this.cameras.main.setBounds(0, 0, map.widthInPixels*4, map.heightInPixels*4)
+        this.cameras.main.setBounds(0, 0, map.widthInPixels*globalScaleFactor, map.heightInPixels*globalScaleFactor)
         this.cameras.main.startFollow(this.bike, true, 0.25, 0.25, 0, 0)
+
+        //define controls
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        keyJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J)
+        keyK = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K)
+
+        //testing
+
     }
 
     //do constantly
     update() {
         //automatically fed time and delta
+        if(keyD.isDown){
+            //this.bike.applyForce(new Phaser.Math.Vector2(0.01, 0))
+            this.bike.setVelocityX(15)
+        }
         
     }
 
