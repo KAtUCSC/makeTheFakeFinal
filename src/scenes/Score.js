802fe7829e8df974a68568784e0de4689a1718cf
KAtUCSC <smway@ucsc.edu>
2024-03-11 11:05:35 -0700

    changed score scene to return to title

--- src/scenes/Score.js
+++ src/scenes/Score.js
@@ -19,13 +19,20 @@ class Score extends Phaser.Scene {
     //make things
     create() {
         //can recieve data
+        //define keys
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+
+        //text
         this.add.bitmapText(game.config.width/2, game.config.height*2/5, 'pixelU', 'Score scene', 64).setOrigin(0.5)
-        this.add.bitmapText(game.config.width/2, game.config.height*3/5, 'pixelU', 'Nothing here yet.', 32).setOrigin(0.5)
+        this.add.bitmapText(game.config.width/2, game.config.height*3/5, 'pixelU', 'Press D to go to title.', 32).setOrigin(0.5)
     }
 
     //do constantly
     update() {
         //automatically fed time and delta
+        if(Phaser.Input.Keyboard.JustDown(keyD)){
+            this.scene.start('titleScene')
+        }
     }
 
     
