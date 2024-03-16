1e25e504f9c08bf9fde4e6f899e809ba28f703b6
KAtUCSC <smway@ucsc.edu>
2024-03-16 15:45:05 -0700

    bike drive and polygon changes

--- src/prefabs/Bike.js
+++ src/prefabs/Bike.js
@@ -6,14 +6,18 @@ class Bike extends Phaser.Physics.Matter.Sprite {
             restitution: 0,
             friction: 0.0,
             frictionAir: 0,
-            frictionStatic: 0,
+            frictionStatic: 0.1,
             vertices: [
               { "x": 47, "y": 12 },
-              { "x": 45, "y": 15 },
-              { "x": 43, "y": 16 },
+              { "x": 46, "y": 14 },
+              { "x": 45, "y": 15.5 },
+              { "x": 44, "y": 16 },
+              { "x": 42, "y": 16 },
               { "x": 35, "y": 12 },
               { "x": 28, "y": 16 },
-              { "x": 25, "y": 15 },
+              { "x": 26, "y": 16 },
+              { "x": 25, "y": 15.5 },
+              { "x": 24, "y": 14 },
               { "x": 23, "y": 12 },
               { "x": 30, "y": 3 },
               { "x": 40, "y": 3 }
@@ -26,7 +30,7 @@ class Bike extends Phaser.Physics.Matter.Sprite {
         this.xAcceleration = 20
         this.xControlSpeed = 10
         this.maxSpeed = 15
-        this.minSpeed = 0
+        this.minSpeed = 1
         this.angleSpeed = 10
         this.angleAcceleration = 1
 
@@ -44,6 +48,7 @@ class Bike extends Phaser.Physics.Matter.Sprite {
         //collision testing
         scene.matter.world.on('collisionactive', this.groundSetter, this)
         //scene.matter.world.on('collisionend', this.groundStopper, this)
+        scene.matter.world.on('collisionstart', this.checkAngle, this)
 
         //testbed
         this.end = false
@@ -55,15 +60,20 @@ class Bike extends Phaser.Physics.Matter.Sprite {
             //reset air timer
             this.airTime.elapsed = 0
             this.airTime.hasDispatched = false
-
+        }
+        
+        if(this.airTime.elapsed < 150) {
+            //using the air timer instead of the grounded flag allows you to keep driving and keep speed after bouncing on the ground
+            //makes movement more consistent and smoother
             //run the player's speed input function
             this.updateTargetSpeed(this.end)
             let bikeSpeed = this.getVelocity().x
             //approach that speed
             //the -0.2 is to prevent it from trying to move at 0 speed when the bike moves slightly backwards from bouncing
-            if(bikeSpeed < this.targetSpeed) {
-                let accerlationGoal = Math.min(this.xAcceleration/100, (this.targetSpeed - bikeSpeed)/10)
-                this.applyForceFrom({x: this.x - this.height/2, y: this.y}, {x: accerlationGoal/10, y: -0.001})
+            if(true) {//bikeSpeed < this.targetSpeed
+                let accerlationGoal = Math.min(this.xAcceleration/100, Math.max(-this.xAcceleration/300, (this.targetSpeed - bikeSpeed)/10))
+                //console.log(this.xAcceleration/100, (this.targetSpeed - bikeSpeed)/10)
+                this.applyForceFrom({x: this.x, y: this.y}, {x: accerlationGoal/10, y: 0}) // - this.height/2
             }
         } else if(this.airTime.elapsed > 200) {
             let currentSpin = this.getAngularVelocity()
@@ -91,15 +101,17 @@ class Bike extends Phaser.Physics.Matter.Sprite {
         }
     }
 
-    groundStopper(matterCollide) {
-        for (let i = 0; i < matterCollide.pairs.length; i++) {
-            //console.log(matterCollide.pairs[i])
-            const bodyA = matterCollide.pairs[i].bodyA
-            const bodyB = matterCollide.pairs[i].bodyB
-            if((bodyA.label === 'bike' && bodyB.label === 'ground') ||
-            (bodyB.label === 'bike' && bodyA.label === 'ground')) {
-                this.grounded = false
-            }
+    checkAngle(matterCollide, bodyA, bodyB) {
+        let angle = 0
+        if(bodyA.label === 'bike') {
+            angle = bodyA.angle
+        } else {
+            angle = bodyB.angle
+        }
+        angle = (2*angle/Math.PI)%4
+        console.log(angle)
+        if(Math.abs(angle) > 1 && Math.abs(angle) < 3) {
+            console.log('CRASH', angle)
         }
     }
 
